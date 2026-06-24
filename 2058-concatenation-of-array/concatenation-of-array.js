1e2178f8e9a3a07e169b7c4137dcb6a8d7b20105
razikrazzi <154303420+razikrazzi@users.noreply.github.com>
2026-06-24 10:51:48 +0530

    Time: 1 ms (58.93%) | Memory: 56.8 MB (91.14%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    
+let num2 = nums.slice();
+let num3 = nums.concat(num2);
+
+return num3
+};
